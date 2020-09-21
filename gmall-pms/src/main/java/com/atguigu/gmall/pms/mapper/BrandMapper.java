2591382a80cb8b873d80cd5135a19d5f81a51b45
pldxnh <2156938896@qq.com>
2020-09-21 19:54:22 +0800

    成功搭建pms和sms

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/BrandMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.BrandEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 品牌
+ * 
+ * @author wh
+ * @email wh@atguigu.com
+ * @date 2020-09-21 18:53:57
+ */
+@Mapper
+public interface BrandMapper extends BaseMapper<BrandEntity> {
+	
+}
---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/SkuAttrValueMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.SkuAttrValueEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * sku销售属性&值
+ * 
+ * @author wh
+ * @email wh@atguigu.com
+ * @date 2020-09-21 18:53:57
+ */
+@Mapper
+public interface SkuAttrValueMapper extends BaseMapper<SkuAttrValueEntity> {
+	
+}
